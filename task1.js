05f79b826daf50a769b4e778f8e3e42ea1ccde53
avistheripper <bboyxns@mail.ru>
2019-11-23 16:17:13 +0300

    Task1.1

--- task1.js
+++ task1.js
@@ -1,1 +1,12 @@
-// Initial commit
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    const reversed = input.split("").reverse().join("");
+    console.log(reversed);
+});
+
